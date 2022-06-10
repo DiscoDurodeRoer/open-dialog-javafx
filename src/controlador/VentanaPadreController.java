4fce919d893bf112ac2c1ce3a8ae4bc0a756566b
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-10 17:39:49 +0200

    open dialog javafx

---
+++ src/controlador/VentanaPadreController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fernando
+ */
+public class VentanaPadreController implements Initializable {
+
+    @FXML
+    private Button btnAbrir;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void abrirVentanaHija(ActionEvent event) {
+
+        try {
+
+            // Cargo la vista
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/VentanaHijaVista.fxml"));
+
+            // Cargo la ventana
+            Parent root = loader.load();
+
+            // Cojo el controlador
+            //PersonaDialogControlador controlador = loader.getController();
+            //controlador.initAtributtes(personas);
+            // Creo el Scene
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+
+    }
+
+}
